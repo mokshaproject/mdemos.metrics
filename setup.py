c44db5f64842f600a0e79f692d7e8fd21f4d8190
Ralph Bean <rbean@redhat.com>
2012-03-20 14:42:43 -0400

    Preparing for pypi.

---
+++ setup.py
@@ -0,0 +1,41 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    name="mdemos.metrics",
+    version="0.1.0b",
+    release="1",
+    url="http://moksha.fedorahosted.org",
+    description="Moksha Metrics App",
+    long_description="",
+    author="Luke Macken",
+    author_email="lmacken@redhat.com",
+    license="ASL 2.0",
+    packages=['mdemos', 'mdemos.metrics'],
+    namespace_packages=['mdemos'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "moksha>=0.7.0a",
+    ],
+    entry_points={
+        'moksha.stream': (
+            'moksha_metrics = mdemos.metrics.streams:MokshaMetricsProducer',
+        ),
+        'moksha.consumer': (
+            'moksha_message_metrics = mdemos.metrics.consumers:MokshaMessageMetricsConsumer',
+        ),
+        'moksha.widget': (
+            'MokshaMemoryUsageWidget = mdemos.metrics.widgets:MokshaMemoryUsageWidget',
+            'MokshaCPUUsageWidget = mdemos.metrics.widgets:MokshaCPUUsageWidget',
+            'MokshaMessageMetricsWidget = mdemos.metrics.widgets:MokshaMessageMetricsWidget',
+        ),
+        'moksha.global': (
+            'moksha_socket = moksha.api.widgets:moksha_socket',
+        ),
+    }
+)
